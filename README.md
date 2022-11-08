9a7f7fbdbabe4bf6e34361cf90030517016597d9
naufalabiyyu <foreignme7@gmail.com>
2022-11-08 16:43:19 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Description
+
+Projek ini merupakan projek latihan pada modul Firebase di [Belajar Fundamental Aplikasi Flutter](https://www.dicoding.com/academies/195). Pada projek ini mengunakan fitur-fitur pada Firebase seperti Firebase Authentication dan Firebase Firestore.
+
+## Development Setup
+
+Clone the repository and run the following commands:
+
+```
+flutter pub get
+flutter run
+```
+
+## Source
+
+[Dicoding Academy](https://www.dicoding.com/academies/14)
+
+## Screenshot
+
+<img src="assets/screenshot/chatapp.png" height="500em" width="1000em/> 
---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,129 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dicoding_chatting/pages/login_page.dart';
+import 'package:dicoding_chatting/widgets/message_bubble.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  static const String id = 'chat_page';
+
+  ChatPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+  late User? _activeUser;
+  final _messageTextController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try {
+      _activeUser = _auth.currentUser;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat Room'),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.close),
+              tooltip: 'Logout',
+              onPressed: () async {
+                final navigator = Navigator.of(context);
+                await _auth.signOut();
+
+                navigator.pushReplacementNamed(LoginPage.id);
+              })
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                stream: _firestore
+                    .collection('messages')
+                    .orderBy('dateCreated', descending: true)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return ListView(
+                      reverse: true,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8,
+                        vertical: 8,
+                      ),
+                      children: snapshot.data!.docs.map((document) {
+                        final data = document.data();
+                        final String messageText = data['text'];
+                        final String messageSender = data['sender'];
+                        return MessageBubble(
+                          sender: messageSender,
+                          text: messageText,
+                          isMyChat: messageSender == _activeUser?.email,
+                        );
+                      }).toList());
+                },
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageTextController,
+                    decoration: const InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                MaterialButton(
+                  color: Theme.of(context).primaryColor,
+                  textTheme: ButtonTextTheme.primary,
+                  onPressed: () async {
+                    _firestore.collection('messages').add({
+                      'text': _messageTextController.text,
+                      'sender': _activeUser!.email,
+                      'dateCreated': Timestamp.now(),
+                    });
+
+                    _messageTextController.clear();
+                  },
+                  child: const Text('SEND'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _messageTextController.dispose();
+    super.dispose();
+  }
+}
